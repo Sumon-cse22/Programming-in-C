d50a7c3a8ad7a2eef0f62222aca829592934b6fc
Sumon-cse22 <saruarjahansumon3@gmail.com>
2024-08-28 22:28:52 +0600

    code updates

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,6 +4,7 @@ int main()
     printf("Hello World!\n");
     printf("Hello Gazipur\n");
     printf("Hello DUET\n");
-    printf("Hello Sumon");
+    printf("Hello Sumon\n");
+    printf("Hello CSE");
     return 0;
 }
